41c880460f78313a66c0037980f478f11a0fc81e
dothack2k7 <erik.castillo@inacapmail.cl>
2016-05-09 13:46:53 -0300

    extraviado, persona movidos a sus controladores
    se movieron cosas

--- src/Controlador/CtrlIngExtraviado.java
+++ src/Controlador/CtrlIngExtraviado.java
@@ -1,25 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controlador;
 
 import Modelo.Extraviado;
 import Modelo.ExtraviadoVO;
 import Modelo.ExtraviadoDAO;
+import Vista.S0_Principal;
 import Vista.S1_IngresarExtraviado;
 import java.awt.event.*;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author DerKow
- */
 public class CtrlIngExtraviado implements ActionListener,KeyListener {
-    S1_IngresarExtraviado vistaIngreso;
-    Extraviado extraviado;
+    private S1_IngresarExtraviado vistaIngreso;
+    private S0_Principal vistaPrincipal;
+    private CtrlIngExtraviado controladorExtraviado;
+    private Extraviado extraviado;
     
     public CtrlIngExtraviado(S1_IngresarExtraviado vistaIngreso, Extraviado modeloIngreso) {
         this.vistaIngreso = vistaIngreso;
@@ -29,7 +23,6 @@ public class CtrlIngExtraviado implements ActionListener,KeyListener {
         this.vistaIngreso.radio_noAplica.addActionListener(this);
         this.vistaIngreso.radio_rut.addActionListener(this);
         this.vistaIngreso.radio_pasaporte.addActionListener(this);
-        
     }
     
     public void Iniciar() {
@@ -39,8 +32,9 @@ public class CtrlIngExtraviado implements ActionListener,KeyListener {
         vistaIngreso.setLocationRelativeTo(null);
         vistaIngreso.setVisible(true);
         vistaIngreso.setResizable(false);
-        extraviado.habilitarExtraviado(false);
+        habilitarExtraviado(false);
         vistaIngreso.radio_noAplica.setSelected(true);
+        
     }
     
     public void actionPerformed(ActionEvent e) {
@@ -58,13 +52,13 @@ public class CtrlIngExtraviado implements ActionListener,KeyListener {
                 extraviadovo.setContextura_ex("");
                 extraviadovo.setPeso_ex(Integer.parseInt("0"));
                 extraviadovo.setComentario_ex(vistaIngreso.textarea_comentario.getText());
-                extraviado.ingresarExtraviado(extraviadovo);
+                ingresarExtraviado(extraviadovo);
             } catch(Exception ex) {
                 JOptionPane.showMessageDialog(null, ex);
             }
         } else if(vistaIngreso.btn_limpiar == e.getSource()) {
             try {
-               extraviado.limpiarCampos();
+               limpiarCampos();
             } catch(Exception ex) {
                 JOptionPane.showMessageDialog(null, ex);
             }
@@ -72,17 +66,17 @@ public class CtrlIngExtraviado implements ActionListener,KeyListener {
         
         /*habilita campo de texto rut,pasaporte,aplica al momento de seleccionar uno,sino queda desabilitado*/
         if (vistaIngreso.radio_rut.isSelected()) {
-            extraviado.habilitarExtraviado(true);
+            habilitarExtraviado(true);
             vistaIngreso.txt_radioOption.requestFocus();
             vistaIngreso.txt_radioOption.setText("");
         }
         if (vistaIngreso.radio_pasaporte.isSelected()) {
-            extraviado.habilitarExtraviado(true);
+            habilitarExtraviado(true);
             vistaIngreso.txt_radioOption.requestFocus();
             vistaIngreso.txt_radioOption.setText("");
         }
         if (vistaIngreso.radio_noAplica.isSelected()) {
-            extraviado.habilitarExtraviado(false);
+            habilitarExtraviado(false);
             vistaIngreso.txt_radioOption.setText("");
         }
         
@@ -113,5 +107,76 @@ public class CtrlIngExtraviado implements ActionListener,KeyListener {
         }catch(Exception ex) {
             
         }
+    }
+    
+    public void ingresarExtraviado(ExtraviadoVO extraviadovo) {
+        ExtraviadoDAO extraviadodao;
+        if(vistaIngreso.buttonGroup1.getSelection() == null || !vistaIngreso.txt_nombre.getText().trim().equals("") || !vistaIngreso.txt_apellidos.getText().trim().equals("") || vistaIngreso.combo_cOjos.getSelectedIndex() > 0 || vistaIngreso.combo_cPelo.getSelectedIndex() > 0 || vistaIngreso.combo_cPiel.getSelectedIndex() > 0 || vistaIngreso.combo_contextura.getSelectedIndex() > 0){
+            if(vistaIngreso.radio_rut.isSelected() == true && !vistaIngreso.txt_radioOption.getText().trim().equals("")){
+                if(validarRut(vistaIngreso.txt_radioOption.getText()) == true){
+                    extraviadodao = new ExtraviadoDAO();
+                    extraviadodao.insertarExtraviado(extraviadovo);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Rut ingresado es incorrecto.","Advertencia",JOptionPane.WARNING_MESSAGE);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Debe ingresar el rut.","Advertencia",JOptionPane.WARNING_MESSAGE);
+            }
+            
+            if(vistaIngreso.radio_pasaporte.isSelected() == true && !vistaIngreso.txt_radioOption.getText().trim().equals("")){
+                extraviadodao = new ExtraviadoDAO();
+                extraviadodao.insertarExtraviado(extraviadovo);
+            } else {
+                JOptionPane.showMessageDialog(null, "Debe ingresar el pasaporte.","Advertencia",JOptionPane.WARNING_MESSAGE);
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Hay campos que no fueron ingresados.","Advertencia",JOptionPane.WARNING_MESSAGE);
+        }
+    }
+    
+    /*limpia todos los campos del formulario*/
+    public void limpiarCampos(){      
+        /*datos extraviado*/
+        habilitarExtraviado(false);
+        
+        vistaIngreso.radio_noAplica.setSelected(true);
+        vistaIngreso.txt_radioOption.setText("");
+        vistaIngreso.txt_nombre.setText("");
+        vistaIngreso.txt_apellidos.setText("");
+        
+        vistaIngreso.combo_cOjos.setSelectedIndex(0);
+        vistaIngreso.combo_cPelo.setSelectedIndex(0);
+        vistaIngreso.combo_cPiel.setSelectedIndex(0);
+        vistaIngreso.combo_contextura.setSelectedIndex(0);
+        
+        vistaIngreso.txt_altura.setText("");
+        vistaIngreso.textarea_comentario.setText("");
+        
+    }
+    
+    public void habilitarExtraviado(boolean sw) {
+        vistaIngreso.txt_radioOption.setEnabled(sw);
+    }
+    
+    public static boolean validarRut(String rut) {
+        boolean validacion = false;
+        try {
+            rut = rut.toUpperCase();
+            rut = rut.replace(".", "");
+            rut = rut.replace("-", "");
+            int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
+
+            char dv = rut.charAt(rut.length() - 1);
+
+            int m = 0, s = 1;
+            for (; rutAux != 0; rutAux /= 10) {
+                s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+            }
+            if (dv == (char) (s != 0 ? s + 47 : 75)) {
+                validacion = true;
+            }
+        } catch(Exception e) {
+        }
+        return validacion;
     }
 }
